d3dee97ff59254b10c600bbcadef4e6ff9783b2e
Jessica Turner <jessica.turner@durham.ac.uk>
2017-02-15 21:30:17 +0530

    add model file

---
+++ Z_MUE_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.0.33
+# Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
+# Date: Wed 15 Feb 2017 21:11:55
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
